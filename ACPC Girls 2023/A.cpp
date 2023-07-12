4b81a96006e56ea517fcb0148fb39af0d84182f0
Alaa <alaaahmed2032002@gmail.com>
2023-07-12 10:35:48 +0300

    added

---
+++ ACPC Girls 2023/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+typedef long long ll; 
+using namespace std; 
+int main()
+{
+    string s, newS = "";
+    cin >> s;
+    newS += s[0];
+
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (s[i] >= 'a')
+            newS += s[i]; 
+        else {
+            reverse(newS.begin(), newS.end());
+            newS.front() = toupper(newS.front());
+            newS.back() = tolower(newS.back()); 
+            cout << newS << " ";
+            newS = "";
+            newS += s[i]; 
+        }
+      
+    }
+    reverse(newS.begin(), newS.end());
+    newS.front() = toupper(newS.front());
+    newS.back() = tolower(newS.back());
+    cout << newS; 
+}
